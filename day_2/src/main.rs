d667268067ce9ab78d8f7f0b92128eed6e012b6f
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-03 00:06:30 +0100

    Complete day 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("src/input.txt").unwrap();
+    let instructions = input.split('\n').filter(|&line| line != "").map(|line| {
+        let split: Vec<_> = line.split(' ').collect();
+        (split[0], split[1].parse::<i32>().unwrap())
+    });
+
+    let mut depth = 0;
+    let mut hoz = 0;
+
+    for (direction, distance) in instructions {
+        match direction {
+            "forward" => hoz += distance,
+            "up" => depth -= distance,
+            "down" => depth += distance,
+            _ => panic!("Unknown direction {}", direction),
+        }
+    }
+
+    println!("{:?}", depth * hoz);
 }

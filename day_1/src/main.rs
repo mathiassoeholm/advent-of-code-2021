e5a15bafebbeac557e15cf71fa4f7534a5f19782
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-02 23:39:39 +0100

    Complete second part of day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -3,20 +3,23 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("src/input.txt").unwrap();
     let mut result = 0;
-    let mut prev = None;
+    let mut prev_three = Vec::with_capacity(3);
     for item in input.split("\n") {
         if item == "" {
             continue;
         }
 
         let item: i32 = item.parse().unwrap();
-        if let Some(prev_item) = prev {
-            if item > prev_item {
+        if prev_three.len() == 3 {
+            let prev_two_sum: i32 = prev_three.iter().skip(1).sum();
+            if prev_two_sum + item > prev_three.iter().sum() {
                 result += 1;
             };
+
+            prev_three.remove(0);
         };
 
-        prev = Some(item);
+        prev_three.push(item);
     }
     println!("{}", result);
 }

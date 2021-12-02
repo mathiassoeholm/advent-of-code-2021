c8329c1285e68bbad15e39f645a33469a501657c
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-02 23:23:25 +0100

    Complete the first challenge

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("src/input.txt").unwrap();
+    let mut result = 0;
+    let mut prev = None;
+    for item in input.split("\n") {
+        if item == "" {
+            continue;
+        }
+
+        let item: i32 = item.parse().unwrap();
+        if let Some(prev_item) = prev {
+            if item > prev_item {
+                result += 1;
+            };
+        };
+
+        prev = Some(item);
+    }
+    println!("{}", result);
 }

91c3ddc4654b67968a269677d62a300ab7f3563f
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-05 09:47:08 +0100

    Almost done with part 1 of day 5

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 struct Point(i32, i32);
 struct Line(Point, Point);
@@ -5,6 +7,7 @@ struct Line(Point, Point);
 fn main() {
     let input = std::fs::read_to_string("src/input.txt").unwrap();
     let lines = input.split('\n');
+
     let lines = lines.map(|line| {
         let mut split = line.split(" -> ");
         let mut start = split
@@ -22,10 +25,30 @@ fn main() {
             Point(end.next().unwrap(), end.next().unwrap()),
         )
     });
+
+    let mut overlaps = HashMap::new();
+
     for Line(start, end) in lines {
         println!("start {:?}", start);
         println!("end {:?}", end);
+        let is_horizontal = start.0 == end.0;
+        let is_vertical = start.1 == end.1;
+
+        if !is_vertical && !is_horizontal {
+            continue;
+        }
+
+        for x in start.0..(end.0 + 1) {
+            for y in start.1..(end.1 + 1) {
+                let key = format!("{},{}", x, y);
+                overlaps.insert(key.clone(), overlaps.get(&key).unwrap_or(&0) + 1);
+            }
+        }
     }
+
+    let result: i32 = overlaps.values().filter(|&&v| v > 1).map(|_| 1).sum();
+
+    println!("{}", result);
     // let lines = lines.map(||)
     // parse lines to
     // lines = Vec((start: (x:0,y:9), end: (x:5,y:9) ...)

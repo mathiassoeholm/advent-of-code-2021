ba386656a9b34148bd271ded29e9ebab944d64ee
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-05 09:23:28 +0100

    Parse input for day 5

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,3 +1,38 @@
+#[derive(Debug)]
+struct Point(i32, i32);
+struct Line(Point, Point);
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("src/input.txt").unwrap();
+    let lines = input.split('\n');
+    let lines = lines.map(|line| {
+        let mut split = line.split(" -> ");
+        let mut start = split
+            .next()
+            .unwrap()
+            .split(',')
+            .map(|str| str.parse::<i32>().unwrap());
+        let mut end = split
+            .next()
+            .unwrap()
+            .split(',')
+            .map(|str| str.parse::<i32>().unwrap());
+        Line(
+            Point(start.next().unwrap(), start.next().unwrap()),
+            Point(end.next().unwrap(), end.next().unwrap()),
+        )
+    });
+    for Line(start, end) in lines {
+        println!("start {:?}", start);
+        println!("end {:?}", end);
+    }
+    // let lines = lines.map(||)
+    // parse lines to
+    // lines = Vec((start: (x:0,y:9), end: (x:5,y:9) ...)
+    // overlapping = Vec<Vec<>>, fill with 0
+    // for each line
+    //   for each point it overlaps
+    //     overlapping[x][y] += 1;
+    //
+    // result = overlapping.map(v => v.sum()).map(v => v.sum());
 }

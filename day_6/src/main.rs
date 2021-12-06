b896c237bd760bd018338ce2eac7b4403d365310
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-06 08:16:56 +0100

    Compelte part 1 of day 6

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("src/input.txt").unwrap();
+    let mut fish_school: Vec<_> = input
+        .split(',')
+        .map(|age| age.parse::<i32>().unwrap())
+        .collect();
+
+    for _ in 0..80 {
+        let mut new_fish = Vec::new();
+        for fish in fish_school.iter_mut() {
+            if *fish == 0 {
+                *fish = 6;
+                new_fish.push(8);
+            } else {
+                *fish -= 1;
+            }
+        }
+
+        fish_school.append(&mut new_fish);
+    }
+    println!("{:?}", fish_school.len());
 }

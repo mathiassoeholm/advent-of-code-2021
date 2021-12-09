0c8afaaf5781ad8988e620c30b574d90451d1885
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-09 08:51:05 +0100

    Complete part 2 of day 9

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -8,10 +8,10 @@ fn main() {
     let input: Vec<_> = input
         .iter()
         .flat_map(|line| line.split("").filter(|v| !v.is_empty()))
-        .map(|v| v.parse::<i32>().unwrap())
+        .map(|v| v.parse::<u8>().unwrap())
         .collect();
 
-    let mut risk_level_sum = 0;
+    let mut bassin_sizes = Vec::new();
 
     for y in 0..col_size {
         for x in 0..row_size {
@@ -20,31 +20,73 @@ fn main() {
             let left = if x > 0 {
                 input[y * row_size + x - 1]
             } else {
-                i32::MAX
+                10
             };
 
             let top = if y > 0 {
                 input[(y - 1) * row_size + x]
             } else {
-                i32::MAX
+                10
             };
 
             let right = if x < row_size - 1 {
                 input[y * row_size + x + 1]
             } else {
-                i32::MAX
+                10
             };
 
             let bottom = if y < col_size - 1 {
                 input[(y + 1) * row_size + x]
             } else {
-                i32::MAX
+                10
             };
 
             if elem < left && elem < top && elem < right && elem < bottom {
-                risk_level_sum += elem + 1;
+                fn visit_neighbours(
+                    row_size: isize,
+                    col_size: isize,
+                    input: &Vec<u8>,
+                    coordinate: (isize, isize),
+                    nodes: &mut Vec<(isize, isize)>,
+                ) {
+                    let index = coordinate.1 * row_size + coordinate.0;
+
+                    if coordinate.0 < 0
+                        || coordinate.1 < 0
+                        || coordinate.0 >= row_size as isize
+                        || coordinate.1 >= col_size as isize
+                        || input[index as usize] >= 9
+                        || nodes.contains(&coordinate)
+                    {
+                        return;
+                    }
+
+                    nodes.push(coordinate);
+                    for neighbor in [
+                        (coordinate.0 - 1, coordinate.1),
+                        (coordinate.0, coordinate.1 - 1),
+                        (coordinate.0 + 1, coordinate.1),
+                        (coordinate.0, coordinate.1 + 1),
+                    ] {
+                        visit_neighbours(row_size, col_size, input, neighbor, nodes);
+                    }
+                }
+
+                let mut nodes = Vec::new();
+                visit_neighbours(
+                    row_size as isize,
+                    col_size as isize,
+                    &input,
+                    (x as isize, y as isize),
+                    &mut nodes,
+                );
+
+                bassin_sizes.push(nodes.len());
             }
         }
     }
-    println!("{}", risk_level_sum);
+
+    bassin_sizes.sort_by(|a, b| b.cmp(a));
+    let largest: usize = bassin_sizes.iter().take(3).product();
+    println!("{:?}", largest);
 }

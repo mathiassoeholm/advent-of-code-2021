75710e1a6d4219148ba47303158caa2280c8fbc2
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-16 08:42:29 +0100

    Start work on day 16

---
+++ day_16/src/main.rs
@@ -0,0 +1,108 @@
+use std::{cell::RefCell, str::Chars};
+
+enum ParseState {
+    ReadingVersion,
+    ReadingId,
+    Reading,
+}
+
+struct Packet {
+    version: u8,
+    id: u8,
+    value: Option<u32>,
+    subPackets: Option<Vec<Packet>>,
+}
+
+// Packet { version, id, value?, subPackets? }
+
+fn main() {
+    let input = std::fs::read_to_string("src/input.txt").unwrap();
+    let bits = input
+        .split("")
+        .filter(|v| !v.is_empty())
+        .map(|c| match c {
+            "0" => "0000",
+            "1" => "0001",
+            "2" => "0010",
+            "3" => "0011",
+            "4" => "0100",
+            "5" => "0101",
+            "6" => "0110",
+            "7" => "0111",
+            "8" => "1000",
+            "9" => "1001",
+            "A" => "1010",
+            "B" => "1011",
+            "C" => "1100",
+            "D" => "1101",
+            "E" => "1110",
+            "F" => "1111",
+            _ => panic!("Unknown char"),
+        })
+        .collect::<Vec<&str>>()
+        .join("");
+    let bits_to_read = bits.len() - (bits.len() % 4);
+    let mut bits = bits.chars();
+
+    parse_packets(&mut bits, bits_to_read);
+}
+
+fn parse_packets(bits: &mut Chars, bits_to_read: usize) -> Vec<Packet> {
+    let bits_read = RefCell::new(0);
+
+    let mut read_bit = || {
+        *bits_read.borrow_mut() += 1;
+        bits.next().unwrap()
+    };
+    // packets = []
+    let mut packets = Vec::new();
+    // while bits_read < bits_to_read {
+    while *bits_read.borrow() < bits_to_read {
+        let version = format!("{}{}{}", read_bit(), read_bit(), read_bit());
+        let version = u8::from_str_radix(&version, 2).unwrap();
+
+        let id = format!("{}{}{}", read_bit(), read_bit(), read_bit());
+        let id = u8::from_str_radix(&id, 2).unwrap();
+
+        if id == 4 {
+            let mut value = "".to_owned();
+
+            // Read first groups
+            while read_bit() != '0' {
+                value += &read_bit().to_string();
+                value += &read_bit().to_string();
+                value += &read_bit().to_string();
+                value += &read_bit().to_string();
+            }
+
+            // Read last group
+            value += &read_bit().to_string();
+            value += &read_bit().to_string();
+            value += &read_bit().to_string();
+            value += &read_bit().to_string();
+
+            let value = u32::from_str_radix(&value, 2).unwrap();
+
+            println!("Value is {}", value);
+
+            packets.push(Packet {
+                version,
+                id,
+                value: Some(value),
+                subPackets: None,
+            });
+        } else {
+        }
+        // else
+        // subpackets = parse_packets(bits, bits_to_read: 32)
+    }
+
+    // return packets
+
+    // if is literal:
+    // return [Packet...]
+    // else
+    // subpackets = parse_packets()
+
+    packets
+}

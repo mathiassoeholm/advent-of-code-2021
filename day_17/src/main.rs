65b5948f896ffe117bcdf60f6a82c204aeb04780
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-17 09:48:22 +0100

    Complete part 1 of day 17

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -11,13 +11,13 @@ fn main() {
     let min_y = *&captures[3].parse::<i32>().unwrap();
     let max_y = *&captures[4].parse::<i32>().unwrap();
 
-    let mut y_attempts: Vec<_> = (0..10000).collect();
+    let mut y_attempts: Vec<_> = (0..500).collect();
     y_attempts.reverse();
 
     for initial_y_velocity in y_attempts {
         let mut is_in_target_area = false;
 
-        for initial_x_velocity in 1..max_x {
+        for initial_x_velocity in 1..(max_x + 1) {
             // println!(
             //     "Trying with new x {} y {}",
             //     initial_x_velocity, initial_y_velocity
@@ -30,10 +30,12 @@ fn main() {
             let mut y_pos = 0;
             let mut velocity_x = initial_x_velocity;
             let mut velocity_y = initial_y_velocity;
+            let mut max_y_pos = 0;
 
             loop {
                 x_pos += velocity_x;
                 y_pos += velocity_y;
+                max_y_pos = std::cmp::max(y_pos, max_y_pos);
                 velocity_x = std::cmp::max(0, velocity_x - 1);
                 velocity_y -= 1;
 
@@ -43,6 +45,10 @@ fn main() {
                 did_undershoot = velocity_x == 0 && x_pos < min_x;
                 is_in_target_area =
                     x_pos >= min_x && x_pos <= max_x && y_pos >= min_y && y_pos <= max_y;
+
+                if is_in_target_area {
+                    println!("y is {}", max_y_pos);
+                }
 
                 if is_in_target_area || did_overshoot || did_undershoot {
                     break;
@@ -55,7 +61,6 @@ fn main() {
         }
 
         if is_in_target_area {
-            println!("y is {}", initial_y_velocity);
             break;
         }
     }

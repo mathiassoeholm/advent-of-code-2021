1954c280d2fac882410bb620c1f8fb6404c51c96
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-18 22:04:40 +0100

    Complete part 1 of day 18

--- typescript/day_18/main.ts
+++ typescript/day_18/main.ts
@@ -1,20 +1,115 @@
+import fs from "fs";
+import path from "path";
+
 interface SnailNode {
   parent?: SnailNode;
   children: SnailNode[];
   value?: number;
 }
 
+const temp = parseSnailNumber("[[1,2],[5,2],[3,4]]");
 let testSnailNumber = "[[[[0,7],4],[15,[0,13]]],[1,1]]";
 
 const tree = parseSnailNumber(testSnailNumber);
 const nodeToExplode = getPairToExplode(tree, 0);
 const nodeToSplit = getNodeToSplit(tree);
-const leftNode = findNodeInDirection(nodeToSplit!, "left");
-const leftLeftNode = findNodeInDirection(leftNode!, "left");
+const leftNode = findNodeInDirection(nodeToSplit!, "right");
+const leftLeftNode = findNodeInDirection(leftNode!, "right");
 const leftLeftLeftNode = findNodeInDirection(leftLeftNode!, "left");
 const leftLeftLeftLeftNode = findNodeInDirection(leftLeftLeftNode!, "left");
 
-console.log("Hello");
+explodePair(temp.children[1]);
+
+const input = fs.readFileSync(path.join(__dirname, "./input.txt"), "utf-8");
+
+const numbers = input.split("\n").map(parseSnailNumber);
+const result = numbers.slice(1).reduce(addNumbers, numbers[0]);
+
+console.log(convertToString(result));
+console.log(getMagnitude(result));
+
+// console.log(getMagnitude(parseSnailNumber("[[9,1],[1,9]]")));
+
+function getMagnitude(tree: SnailNode): number {
+  if (tree.children.length === 0) {
+    return tree.value!;
+  }
+
+  return (
+    3 * getMagnitude(tree.children[0]) + 2 * getMagnitude(tree.children[1])
+  );
+}
+
+function convertToString(tree: SnailNode): string {
+  if (tree.value != undefined) {
+    return tree.value.toString();
+  }
+
+  return "[" + tree.children.map(convertToString).join(",") + "]";
+}
+
+function addNumbers(aTree: SnailNode, bTree: SnailNode) {
+  const sum: SnailNode = {
+    children: [aTree, bTree],
+  };
+  aTree.parent = sum;
+  bTree.parent = sum;
+
+  let pairToExplode;
+  let nodeToSplit;
+  while (
+    (pairToExplode = getPairToExplode(sum, 0)) !== null ||
+    (nodeToSplit = getNodeToSplit(sum)) !== null
+  ) {
+    if (pairToExplode) {
+      explodePair(pairToExplode);
+    } else if (nodeToSplit) {
+      splitNode(nodeToSplit);
+    }
+  }
+
+  return sum;
+}
+
+function splitNode(node: SnailNode) {
+  const left: SnailNode = {
+    value: Math.floor(node.value! / 2),
+    parent: node,
+    children: [],
+  };
+
+  const right: SnailNode = {
+    value: Math.ceil(node.value! / 2),
+    parent: node,
+    children: [],
+  };
+
+  node.children = [left, right];
+  node.value = undefined;
+}
+
+function explodePair(node: SnailNode) {
+  if (!nodeIsPair(node)) {
+    throw new Error("Node is not a pair!");
+  }
+
+  const leftChild = node.children[0];
+  const rightChild = node.children[1];
+
+  const left = findNodeInDirection(node, "left");
+  const right = findNodeInDirection(node, "right");
+
+  if (left) {
+    left.value! += leftChild.value!;
+  }
+
+  if (right) {
+    right.value! += rightChild.value!;
+  }
+
+  node.value = 0;
+  node.children = [];
+}
 
 function findNodeInDirection(
   node: SnailNode,
@@ -24,10 +119,14 @@ function findNodeInDirection(
   if (goingUp) {
     if (!node.parent) {
       return null;
-    } else if (node.parent.children[0] !== node) {
+    } else if (
+      (dir === "left" && node.parent.children[0] !== node) ||
+      (dir === "right" &&
+        node.parent.children[node.parent.children.length - 1] !== node)
+    ) {
       const childIndex = node.parent!.children.findIndex((c) => c === node);
       return findNodeInDirection(
-        node.parent!.children[childIndex - 1],
+        node.parent!.children[dir === "left" ? childIndex - 1 : childIndex + 1],
         dir,
         false
       );
@@ -38,7 +137,9 @@ function findNodeInDirection(
     if (node.value != undefined) {
       return node;
     } else {
-      for (const child of [...node.children].reverse()) {
+      for (const child of dir === "left"
+        ? [...node.children].reverse()
+        : node.children) {
         const n = findNodeInDirection(child, dir, false);
         if (n) {
           return n;
@@ -91,7 +192,7 @@ function nodeIsPair(node: SnailNode) {
 }
 
 function parseSnailNumber(input: string) {
-  const matches = testSnailNumber.matchAll(/(\[|\]|\d+|,)/g);
+  const matches = input.matchAll(new RegExp("(\\[|\\]|\\d+|,)", "g"));
   let root: SnailNode | undefined;
   let currentNode: SnailNode | undefined;
 

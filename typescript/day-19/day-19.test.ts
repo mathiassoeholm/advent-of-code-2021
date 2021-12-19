4871fe4d147ff103be9346fb7cda4f23f751b8f3
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-19 14:53:22 +0100

    Small progress on day 19

--- typescript/day-19/day-19.test.ts
+++ typescript/day-19/day-19.test.ts
@@ -1,7 +1,35 @@
-import { getAllOrientations } from "./day-19";
+import { getAllOrientations, getAllOrientationsForList } from "./day-19";
 
 describe("day 19", () => {
-  it("asfdfa", () => {
-    expect(getAllOrientations(-1, -1, 1)).toBe(true);
+  it("can get different orientations for a single position", () => {
+    expect(getAllOrientations(-1, -1, 1)).toEqual(
+      expect.arrayContaining([
+        [-1, -1, 1],
+        [1, -1, 1],
+        [-1, -1, -1],
+        [1, 1, -1],
+        [1, 1, 1],
+      ])
+    );
+  });
+
+  it("can get different orientations of a list of positions", () => {
+    expect(
+      getAllOrientationsForList([
+        [-1, -1, 1],
+        [-2, -2, 2],
+      ])
+    ).toEqual(
+      expect.arrayContaining([
+        expect.arrayContaining([
+          [-1, -1, 1],
+          [1, -1, 1],
+        ]),
+        expect.arrayContaining([
+          [-2, -2, 2],
+          [2, -2, 2],
+        ]),
+      ])
+    );
   });
 });
--- typescript/day-19/day-19.ts
+++ typescript/day-19/day-19.ts
@@ -1,3 +1,61 @@
-export function getAllOrientations(x: number, y: number, z: number) {
-  return true;
+export type Position = [x: number, y: number, z: number];
+
+export function getAllOrientations(
+  x: number,
+  y: number,
+  z: number
+): Position[] {
+  const positions: Array<Position> = [];
+
+  for (const i of [-1, 1]) {
+    for (const j of [-1, 1]) {
+      for (const k of [-1, 1]) {
+        positions.push([x * i, y * j, z * k]);
+        positions.push([z * i, x * j, y * k]);
+        positions.push([y * i, z * j, x * k]);
+      }
+    }
+  }
+
+  return positions;
 }
+
+export function getAllOrientationsForList(
+  positions: Position[]
+): Array<Position[]> {
+  return positions.map((p) => getAllOrientations(p[0], p[1], p[2]));
+}
+
+function solveChallenge() {
+  // input = read file
+  // const absoluteBeacons = new Set with all beacons from scanner 0
+  // const relativeBeacons = [scanner][beacon]
+  //
+  // for each beaconList in relativeBeacons
+  //   const orientations = getAllOrientations(beaconList)
+  //   for each beaconListOriented in orientations
+  //     for each beaconRel in beaconListOriented
+  //       for each beaconAbs in absoluteBeacons
+  //         const offset - beaconAbs - beaconRel
+  //         const relBeaconsOffsetted = beaconListOriented.map(use offset)
+  //         if relBeaconsOffsetted.filter(b => b is in absoluteBeacons).count >= 12
+  //            we have a winner!
+  //            Add relBeaconsOffsetted to absoluteBeacons
+  //
+  // result is = size of set absoluteBeacons
+}
+
+// let restScanners = [all others]
+// for each scanner a in scannersRelativeTo0
+// for each scanner b in restScanners
+//   for all orientations of b
+//     for all beacons in b
+//       for all beacons in a
+//         offset all beacons in b by beacon_a-beacon_b
+//         count the overlaps
+//         if overlap >= 12:
+//            we have a pair
+//            calculate scanner b pos relative to scanner a
+//            save in scannersRelativeTo0
+//            remove from restScanners
+//

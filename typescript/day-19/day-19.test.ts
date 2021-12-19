3353df7d88a96d03700730219abaed38afac339b
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-19 22:44:21 +0100

    Solve part 1

--- typescript/day-19/day-19.test.ts
+++ typescript/day-19/day-19.test.ts
@@ -5,15 +5,18 @@ import {
 } from "./day-19";
 import { exampleInput } from "./example-input";
 
+import path from "path";
+import fs from "fs";
+
 describe("day 19", () => {
   it("can get different orientations for a single position", () => {
-    expect(getAllOrientations(-1, -1, 1)).toEqual(
+    expect(getAllOrientations(5, 6, -4)).toEqual(
       expect.arrayContaining([
-        [-1, -1, 1],
-        [1, -1, 1],
-        [-1, -1, -1],
-        [1, 1, -1],
-        [1, 1, 1],
+        [5, 6, -4],
+        [-5, 4, -6],
+        [4, 6, 5],
+        [-4, -6, 5],
+        [-6, -4, -5],
       ])
     );
   });
@@ -28,18 +31,33 @@ describe("day 19", () => {
       expect.arrayContaining([
         expect.arrayContaining([
           [-1, -1, 1],
-          [1, -1, 1],
+          [-2, -2, 2],
         ]),
         expect.arrayContaining([
-          [-2, -2, 2],
+          [1, -1, 1],
           [2, -2, 2],
         ]),
       ])
     );
   });
 
+  it("works with very simple input", () => {
+    const simpleInput =
+      "--- scanner 0 ---\n-618,-824,-621\n-537,-823,-458\n\n--- scanner 1 ---\n686,422,578\n605,423,415";
+    const result = solveChallenge(simpleInput);
+    expect(result).toBe(2);
+  });
+
   it("returns same result as example", () => {
     const result = solveChallenge(exampleInput);
     expect(result).toBe(79);
+  });
+
+  it("works with input.txt", () => {
+    jest.setTimeout(500_000);
+
+    const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+    const result = solveChallenge(input);
+    console.log(result);
   });
 });
--- typescript/day-19/day-19.ts
+++ typescript/day-19/day-19.ts
@@ -11,8 +11,11 @@ export function getAllOrientations(
     for (const j of [-1, 1]) {
       for (const k of [-1, 1]) {
         positions.push([x * i, y * j, z * k]);
-        positions.push([z * i, x * j, y * k]);
+        positions.push([x * i, z * j, y * k]);
+        positions.push([y * i, x * j, z * k]);
         positions.push([y * i, z * j, x * k]);
+        positions.push([z * i, x * j, y * k]);
+        positions.push([z * i, y * j, x * k]);
       }
     }
   }
@@ -23,7 +26,18 @@ export function getAllOrientations(
 export function getAllOrientationsForList(
   positions: Position[]
 ): Array<Position[]> {
-  return positions.map((p) => getAllOrientations(p[0], p[1], p[2]));
+  const lists: Position[][] = [];
+  positions.forEach((p) =>
+    getAllOrientations(p[0], p[1], p[2]).forEach((p, i) => {
+      if (!lists[i]) {
+        lists.push([]);
+      }
+
+      lists[i].push(p);
+    })
+  );
+
+  return lists;
 }
 
 export function solveChallenge(input: string) {
@@ -31,13 +45,62 @@ export function solveChallenge(input: string) {
     text
       .split("\n")
       .slice(1)
-      .map((line) => line.split(",").map((str) => parseInt(str)))
+      .map((line) => line.split(",").map((str) => parseInt(str)) as Position)
   );
 
-  const absoluteBeacons = new Set(beacons[0]);
+  const absoluteBeacons = beacons[0];
   const relativeBeacons = beacons.slice(1);
 
-  console.log(beacons);
+  for (let i = 0; i < 3; i++) {
+    for (const beaconList of relativeBeacons) {
+      console.log("Checking");
+      let continueToNextScanner = false;
+      const orientations = getAllOrientationsForList(beaconList);
+      for (const orientedbeaconList of orientations) {
+        for (const beaconRelative of orientedbeaconList) {
+          for (const beaconAbsolute of absoluteBeacons) {
+            const offset = [
+              beaconAbsolute[0] - beaconRelative[0],
+              beaconAbsolute[1] - beaconRelative[1],
+              beaconAbsolute[2] - beaconRelative[2],
+            ];
+
+            const orientedBeaconListWithOffset = orientedbeaconList.map((p) => [
+              p[0] + offset[0],
+              p[1] + offset[1],
+              p[2] + offset[2],
+            ]);
+
+            const beaconsInCommon = orientedBeaconListWithOffset.filter((b) =>
+              absoluteBeacons.some(
+                (a) => b[0] === a[0] && b[1] === a[1] && b[2] === a[2]
+              )
+            );
+            if (beaconsInCommon.length >= 12) {
+              orientedBeaconListWithOffset
+                .filter((b) => !beaconsInCommon.includes(b))
+                .forEach((p) => {
+                  absoluteBeacons.push(p as Position);
+                });
+              console.log("win!");
+              continueToNextScanner = true;
+            }
+            if (continueToNextScanner) {
+              break;
+            }
+          }
+          if (continueToNextScanner) {
+            break;
+          }
+        }
+        if (continueToNextScanner) {
+          break;
+        }
+      }
+    }
+  }
+
+  console.log(absoluteBeacons);
   // const absoluteBeacons = new Set with all beacons from scanner 0
   // const relativeBeacons = [scanner][beacon]
   //
@@ -53,6 +116,7 @@ export function solveChallenge(input: string) {
   //            Add relBeaconsOffsetted to absoluteBeacons
   //
   // result is = size of set absoluteBeacons
+  return absoluteBeacons.length;
 }
 
 // let restScanners = [all others]

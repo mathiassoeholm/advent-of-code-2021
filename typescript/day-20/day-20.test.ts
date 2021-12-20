c3af8d3a048f7aefa2c7fcfc7336eda8b1600ffe
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-20 07:58:08 +0100

    Print decoded images

--- typescript/day-20/day-20.test.ts
+++ typescript/day-20/day-20.test.ts
@@ -1,19 +1,53 @@
 import path from "path";
 import fs from "fs";
-import { parseInput } from "./day-20";
+import {
+  convertPixelsToNumber,
+  padImage,
+  parseInput,
+  selectSorrounding,
+  solveChallenge,
+} from "./day-20";
+
+const exampleImage = ["#..#.", "#....", "##..#", "..#..", "..###"];
 
 describe("day 20", () => {
   it("can parse input", () => {
     const parsed = parseInput("..#.#\n###.\n\n#.\n.#");
     expect(parsed).toEqual({
       decoder: [".", ".", "#", ".", "#", "#", "#", "#", "."],
-      image: "#.\n.#",
+      image: ["#.", ".#"],
     });
   });
 
-  // it("works with input.txt", () => {
-  //   const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
-  //   const result = solveChallenge(input);
-  //   console.log(result);
-  // });
+  it("can pad image", () => {
+    const padded = padImage(["##", "##"]);
+    expect(padded).toEqual([
+      "......",
+      "......",
+      "..##..",
+      "..##..",
+      "......",
+      "......",
+    ]);
+  });
+
+  it("can select sorrounding pixels", () => {
+    let pixels = selectSorrounding(exampleImage, 2, 2);
+    expect(pixels).toBe("...#...#.");
+
+    pixels = selectSorrounding(exampleImage, 0, 0);
+    expect(pixels).toBe("....#..#.");
+  });
+
+  it("can map pixels to a binary number and do a lookup", () => {
+    const pixels = "...#...#.";
+    const num = convertPixelsToNumber(pixels);
+    expect(num).toBe(34);
+  });
+
+  it("works with input.txt", () => {
+    const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+    const result = solveChallenge(input);
+    console.log(result);
+  });
 });
--- typescript/day-20/day-20.ts
+++ typescript/day-20/day-20.ts
@@ -2,6 +2,67 @@ export function parseInput(input: string) {
   const [decoder, image] = input.split("\n\n");
   return {
     decoder: decoder.split("").filter((c) => c !== "\n"),
-    image,
+    image: image.split("\n"),
   };
+}
+
+export function padImage(image: string[]) {
+  return [
+    ".".repeat(image.length + 4),
+    ".".repeat(image.length + 4),
+    ...image.map((row) => `..${row}..`),
+    ".".repeat(image.length + 4),
+    ".".repeat(image.length + 4),
+  ];
+}
+
+export function selectSorrounding(image: string[], x: number, y: number) {
+  return [
+    [x - 1, y - 1],
+    [x, y - 1],
+    [x + 1, y - 1],
+    [x - 1, y],
+    [x, y],
+    [x + 1, y],
+    [x - 1, y + 1],
+    [x, y + 1],
+    [x + 1, y + 1],
+  ]
+    .map(([x1, y1]) => image[y1]?.[x1] ?? ".")
+    .join("");
+}
+
+export function convertPixelsToNumber(pixels: string) {
+  const binaryString = pixels
+    .split("")
+    .map((pixel) => (pixel === "#" ? "1" : "0"))
+    .join("");
+  return parseInt(binaryString, 2);
+}
+
+export function solveChallenge(input: string) {
+  let { decoder, image } = parseInput(input);
+  for (let i = 0; i < 2; i++) {
+    image = padImage(image);
+    image = decode(image, decoder);
+    printImage(image);
+  }
+}
+
+export function decode(image: string[], decoder: string[]) {
+  let result = image.map(() => "");
+  for (let y = 0; y < image.length; y++) {
+    for (let x = 0; x < image[y].length; x++) {
+      const sorrounding = selectSorrounding(image, x, y);
+      const num = convertPixelsToNumber(sorrounding);
+      const symbol = decoder[num];
+      result[y] += symbol;
+    }
+  }
+
+  return result;
+}
+
+export function printImage(image: string[]) {
+  console.log(image.join("\n"));
 }

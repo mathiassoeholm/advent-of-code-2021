a75f82ac0d7b350ddd89fd4f47333683ef07c16b
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-12-22 10:02:28 +0100

    Implement parseVolume

--- typescript/day-22/day-22.test.ts
+++ typescript/day-22/day-22.test.ts
@@ -1,3 +1,5 @@
+import { isInVolume, parseVolume } from "./day-22";
+
 describe("day 22", () => {
   it("should parse volume", () => {
     expect(parseVolume("on x=-46..7,y=-6..46,z=-50..-1")).toEqual({
@@ -12,11 +14,11 @@ describe("day 22", () => {
   });
 
   it("should check if overlaps", () => {
-    const volume = "on x=-46..7,y=-6..46,z=-50..-1";
-    expect(doesOverlap([-50, 0, 0], volume)).toBe(false);
-    expect(doesOverlap([-46, 0, 0], volume)).toBe(true);
-    expect(doesOverlap([-5, 7, 0], volume)).toBe(true);
-    expect(doesOverlap([-5, 7, 2], volume)).toBe(false);
-    expect(doesOverlap([-5, 47, 0], volume)).toBe(false);
+    const volume = parseVolume("on x=-46..7,y=-6..46,z=-50..-1");
+    expect(isInVolume([-50, 0, 0], volume)).toBe(false);
+    expect(isInVolume([-46, 0, 0], volume)).toBe(true);
+    expect(isInVolume([-5, 7, 0], volume)).toBe(true);
+    expect(isInVolume([-5, 7, 2], volume)).toBe(false);
+    expect(isInVolume([-5, 47, 0], volume)).toBe(false);
   });
 });
--- typescript/day-22/day-22.ts
+++ typescript/day-22/day-22.ts
@@ -1,3 +1,35 @@
 // For each line
 // Run through overlapping cubes
 // toggle them
+
+interface Volume {
+  xMin: number;
+  xMax: number;
+  yMin: number;
+  yMax: number;
+  zMin: number;
+  zMax: number;
+  on: boolean;
+}
+
+export function parseVolume(str: string): Volume {
+  const match = str.match(
+    /^(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)/
+  )!;
+  return {
+    xMin: parseInt(match[2]),
+    xMax: parseInt(match[3]),
+    yMin: parseInt(match[4]),
+    yMax: parseInt(match[5]),
+    zMin: parseInt(match[6]),
+    zMax: parseInt(match[7]),
+    on: match[1] === "on",
+  };
+}
+
+export function isInVolume(
+  [x, y, z]: [number, number, number],
+  volume: Volume
+) {
+  return false;
+}
